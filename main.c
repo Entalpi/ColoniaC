9964253e06a172d7e6af6165aa073434b74372d4
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2019-02-21 15:26:12 +0100

    Added BIDE population model simulation

--- main.c
+++ main.c
@@ -14,6 +14,13 @@
 // Tax collection should be simulated by trying to model how they collected
 // taxes in the early Roman Empire.
 
+// TODO: Refactor into math.h?
+static inline float maxf(const float a, const float b) { return a < b ? b : a; }
+
+static inline float maxi(const uint32_t a, const uint32_t b) {
+  return a < b ? b : a;
+}
+
 static WINDOW *root = NULL; // FIXME: ...
 
 /***** ncurses utility functions *****/
@@ -45,6 +52,17 @@ void destroy_win(WINDOW *win) {
 }
 
 static uint64_t timestep = 0;
+static uint32_t simulation_speed = 1;
+
+static inline uint32_t ms_per_timestep_for(const uint32_t speed) {
+  assert(speed >= 0);
+  switch (speed) {
+  case 0:
+    return 0;
+  default:
+    return maxi(500 - 50 * speed, 50);
+  }
+}
 
 const char *get_year_str(const uint64_t timestep) {
   // TODO: Implement time system
@@ -52,19 +70,29 @@ const char *get_year_str(const uint64_t timestep) {
   return "Year of CON III Grassius & CON I Octavian";
 }
 
-const char* get_season_str(const uint64_t timestep) {
+const char *get_season_str(const uint64_t timestep) {
   // TODO: Implement
-  return "winter"; 
+  return "winter";
 }
 
 struct City {
   char *name;
   uint32_t population;
   //
-  uint32_t food; // TODO: Calories?
+  int32_t food; // TODO: Calories? Negative means starvation?
   //
   int32_t gold; // Pounds of gold (kg??) (negative, debt?? )
   //
+  float birthrate;       // Births / 1'000 pops / timestep
+  float births;          // Accumulated from previous timesteps
+  float deathrate;       // Deaths / 1'000 pops / timestep
+  float deaths;          // Accumulated from previous timesteps
+  float immigrationrate; // Immigration in # pops / 1'000 pops / timestep
+  float immigrations;    // Accumulated from previous timesteps
+  float emmigrationrate; // Emmigration in # pops / 1'000 pops / timestep
+  float emmigrations;    // Accumulated from previous timesteps
+  //
+  // TODO: Ensure order independence of the execution of effects
   struct Effect *effects;
   size_t num_effects;
 };
@@ -78,6 +106,7 @@ struct Effect {
 
 void farm_tick_effect(struct Effect *effect) {
   assert(effect);
+  assert(effect->resrc);
   // TODO: Formula for typical farm output in number of calories around in Roman
   // times
   if (timestep % 2 == 0) {
@@ -91,7 +120,7 @@ void pops_eating_tick_effect(struct Effect *effect) {
   struct City *c = (struct City *)effect->resrc;
   // TODO: Formula for typical human consumption counted in calories in Roman
   // times
-  const uint32_t consumption = (uint32_t)(c->population * 0.00001f);
+  const uint32_t consumption = c->population * 0.0001f;
   c->food -= consumption;
 }
 
@@ -102,15 +131,26 @@ void pops_population_tick_effect(struct Effect *effect) {
   // TODO: Formula for population growth depending on food/etc in Roman times
   // TODO: Uniform random value:
   // https://codereview.stackexchange.com/questions/159604/uniform-random-numbers-in-an-integer-interval-in-c
-  const float x = rand() / (float)RAND_MAX;
-  const uint32_t ppl = (uint32_t)(rand() / (float)RAND_MAX * 5.0f);
-  if (x <= 0.5f) {
-    c->population -= ppl;
-  } else {
-    c->population += ppl;
-  }
+  c->births += c->birthrate * c->population;
+  c->immigrations += c->immigrationrate * c->population;
+  c->deaths += c->deathrate * c->population;
+  c->emmigrations += c->emmigrationrate * c->population;
+  c->population += c->births + c->immigrations - c->deaths - c->emmigrations;
+  mvclrprintw(root, 10, 0, "Births: %f", c->births);
+  mvclrprintw(root, 11, 0, "Deaths: %f", c->deaths);
+  mvclrprintw(root, 12, 0, "Immigrations: %f", c->immigrations);
+  mvclrprintw(root, 13, 0, "Emmigrations: %f", c->emmigrations);
+  // Accumulate the remainder to the next timestep
+  c->births = maxf(c->births - (int)c->births, 0.0f);
+  c->deaths = maxf(c->deaths - (int)c->deaths, 0.0f);
+  c->immigrations = maxf(c->immigrations - (int)c->immigrations, 0.0f);
+  c->emmigrations = maxf(c->emmigrations - (int)c->emmigrations, 0.0f);
 }
 
+// TODO: Expand this to a decision tree and then a story in which you either
+// fail or get the choice of moving the capital of the Roman Empire to your city
+// or move to Rome and then the game turns in a slow downward turn into decline?
+// Fun?
 void imperator_demands_money(struct Effect *effect) {
   assert(effect);
   assert(effect->resrc);
@@ -174,13 +214,17 @@ void update_ui(const struct City *c) {
               timestep / 24, // TODO: Use the Roman calendar
               timestep % 24, get_season_str(timestep));
   mvclrprintw(root, 3, 0, "Population: %u", c->population);
-  mvclrprintw(root, 4, 0, "Food: %u", c->food);
-  mvclrprintw(root, 5, 0, "Gold: %u", c->gold);
+  mvclrprintw(root, 4, 0, "Food: %i kg", c->food);
+  mvclrprintw(root, 5, 0, "Gold: %i kg", c->gold);
 
   // TODO: Implement controls and menu system
-  mvclrprintw(root, LINES - 1, 0,
-              "Q: menu (quit for now) | C: construction | P: policy");
-  // TODO: Pause, speed step
+  mvclrprintw(
+      root, LINES - 1, 0,
+      "Speed: %u | Q: menu (quit for now) | C: construction | P: policy",
+      simulation_speed);
+  // TODO: Show drastically declining (good) numbers in reddish, yellowish for
+  // stable values, green for increasing (good) values and vice versa for bad
+  // values like deaths
 }
 
 int main() {
@@ -188,7 +232,7 @@ int main() {
   root = initscr(); /* initialize the curses library */
 
   // TOOD: Add help flag with descriptions
-  // TODO: Hard mode = Everything is in Latin. Enjoy.
+  // TODO: Hard mode = Everything is in Latin with Roman measurements. Enjoy.
 
   cbreak();             /* Line buffering disabled pass on everything to me*/
   keypad(stdscr, TRUE); /* For keyboard arrows 	*/
@@ -196,12 +240,16 @@ int main() {
   nodelay(root, true);  /* Make getch non-blocking */
   refresh();
 
-  //  Default city
+  // Default city
   struct City city = {0};
   city.name = "Eboracum";
   city.food = 100;
   city.gold = 100;
   city.population = 5500; // TODO: Several types of people?
+  city.birthrate = 0.001f;
+  city.deathrate = 0.002f;
+  city.emmigrationrate = 0.001f;
+  city.immigrationrate = 0.00205f;
 
   struct Effect farm = {.resrc = &city.food, .duration = FOREVER};
   farm.tick_effect = farm_tick_effect;
@@ -225,7 +273,6 @@ int main() {
 
   bool quit = false;
   char ch = 0;
-  uint32_t MS_PER_TIMESTEP = 100;
 
   struct timeval t0;
   struct timeval t1;
@@ -237,7 +284,9 @@ int main() {
     dt = ((t1.tv_sec * 1000) + (t1.tv_usec / 1000)) -
          ((t0.tv_sec * 1000) + (t0.tv_usec / 1000));
 
-    if (dt >= MS_PER_TIMESTEP) {
+    // TODO: Pass the delta rest onto next timestep
+    const uint32_t ms_per_timestep = ms_per_timestep_for(simulation_speed);
+    if (dt >= ms_per_timestep && ms_per_timestep != 0) {
       next_timestep(&city);
       t0 = t1;
     }
@@ -246,10 +295,34 @@ int main() {
     ch = getch();
     switch (ch) {
     case '1':
-      MS_PER_TIMESTEP = 100;
+      simulation_speed = 1;
       break;
     case '2':
-      MS_PER_TIMESTEP = 50;
+      simulation_speed = 2;
+      break;
+    case '3':
+      simulation_speed = 3;
+      break;
+    case '4':
+      simulation_speed = 4;
+      break;
+    case '5':
+      simulation_speed = 5;
+      break;
+    case '6':
+      simulation_speed = 6;
+      break;
+    case '7':
+      simulation_speed = 7;
+      break;
+    case '8':
+      simulation_speed = 8;
+      break;
+    case '9':
+      simulation_speed = 9;
+      break;
+    case ' ':
+      simulation_speed = 0;
       break;
     case 'q':
       return 0;
