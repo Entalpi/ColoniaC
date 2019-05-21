7ff2798dd22913131de8b51bc6df1bb5f26085dd
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2019-05-21 11:48:32 +0200

    Effects changes, construction over time

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <ncurses.h>
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -367,18 +368,23 @@ static inline bool is_winter(const struct Date *date) {
 }
 
 struct Policy {
-  const char* name_str;
-  const char* description_str;
-  struct Effect* effect;
+  const char *name_str;
+  const char *description_str;
+  struct Effect *effect;
 };
 
 struct Construction {
+  bool construction_finished;
+  bool maintained;
   const char *name_str;
   const char *description_str;
+  float
+      construction_cost; // Cost in gold per timestep during construction period
   float cost;
   float maintenance;
   struct Effect *effect;
-  size_t num_effects; // Construction variants
+  size_t num_effects;       // Construction variants
+  size_t construction_time; // Time to build in timesteps (days)
 };
 
 struct City {
@@ -417,7 +423,7 @@ struct City {
   size_t num_constructions;
   size_t num_constructions_capacity;
   // Policies
-  struct Construction* policies;
+  struct Construction *policies;
   size_t num_policies;
   size_t num_polciies_capacity;
 };
@@ -427,15 +433,17 @@ struct City {
 struct Effect {
   char *name_str;        // Human readable name of the effect
   char *description_str; // Human readable description of the effect
-  int64_t duration;      // Negative for forever, 0 = done/inactive
-  void (*tick_effect)(const struct City *c, struct City *c1);
+  int64_t duration; // Negative for forever, 0 = done/inactive, timesteps left
+  void *arg;        // Custom argument provided
+  void (*tick_effect)(struct Effect *e, const struct City *c, struct City *c1);
 };
 
 // TODO: Use realloc instead
 struct Effect *city_add_effect(struct City *c, const struct Effect e) {
   if (c->num_effects + 1 > c->num_effects_capacity) {
     c->num_effects_capacity += 10;
-    c->effects = realloc(c->effects, sizeof(struct Effect) * c->num_effects_capacity);
+    c->effects =
+        realloc(c->effects, sizeof(struct Effect) * c->num_effects_capacity);
   }
   c->effects[c->num_effects++] = e;
   return &c->effects[c->num_effects - 1];
@@ -445,7 +453,9 @@ struct Construction *city_add_construction(struct City *c,
                                            const struct Construction con) {
   if (c->num_constructions + 1 > c->num_constructions_capacity) {
     c->num_constructions_capacity += 10;
-    c->constructions = realloc(c->constructions, sizeof(struct Construction) * c->num_constructions_capacity);
+    c->constructions =
+        realloc(c->constructions,
+                sizeof(struct Construction) * c->num_constructions_capacity);
   }
   c->constructions[c->num_constructions++] = con;
   return &c->constructions[c->num_constructions - 1];
@@ -456,7 +466,9 @@ city_add_construction_project(struct City *c, const struct Construction con) {
   if (c->num_construction_projects + 1 >
       c->num_construction_projects_capacity) {
     c->num_construction_projects_capacity += 10;
-    c->construction_projects = realloc(c->construction_projects, sizeof(struct Construction) * c->num_construction_projects_capacity);
+    c->construction_projects = realloc(
+        c->construction_projects,
+        sizeof(struct Construction) * c->num_construction_projects_capacity);
   }
   c->construction_projects[c->num_construction_projects++] = con;
   return &c->construction_projects[c->num_construction_projects - 1];
@@ -498,7 +510,7 @@ void simulate_next_timestep(const struct City *c, struct City *c1) {
 
   // Compute effects affecting the change of rate
   for (size_t i = 0; i < c1->num_effects; i++) {
-    c1->effects[i].tick_effect(c, c1);
+    c1->effects[i].tick_effect(&c1->effects[i], c, c1);
     if (c1->effects[i].duration == FOREVER) {
       continue;
     }
@@ -512,7 +524,14 @@ void simulate_next_timestep(const struct City *c, struct City *c1) {
   }
 
   for (size_t i = 0; i < c1->num_constructions; i++) {
-    c1->constructions[i].effect->tick_effect(c, c1);
+    struct Construction *con = &c1->constructions[i];
+    if (!con->construction_finished) {
+      continue;
+    }
+    if (!con->maintained) {
+      continue;
+    }
+    con->effect->tick_effect(con->effect, c, c1);
   }
 
   // Compute changes during this timestep
@@ -525,53 +544,62 @@ void simulate_next_timestep(const struct City *c, struct City *c1) {
   increment_date(&date);
 }
 
-void building_maintenance_tick_effect(const struct City *c, struct City *c1) {
+void building_maintenance_tick_effect(struct Effect *e, const struct City *c,
+                                      struct City *c1) {
   for (size_t i = 0; i < c->num_constructions; i++) {
     c1->gold_usage += c->constructions[i].maintenance;
   }
 }
 
-void farm_tick_effect(const struct City *c, struct City *c1) {
+void farm_tick_effect(struct Effect *e, const struct City *c, struct City *c1) {
   if (is_winter(&date)) {
     return;
   }
   c1->food_production += 0.1f;
 }
 
-void aqueduct_tick_effect(const struct City *c, struct City *c1) {
+void aqueduct_tick_effect(struct Effect *e, const struct City *c,
+                          struct City *c1) {
   c1->diplomatic_capacity += 1;
   c1->immigrationrate += 0.000001f;
 }
 
-void basilica_tick_effect(const struct City *c, struct City *c1) {
+void basilica_tick_effect(struct Effect *e, const struct City *c,
+                          struct City *c1) {
   c1->political_capacity += 1;
 }
 
-void forum_tick_effect(const struct City *c, struct City *c1) {
+void forum_tick_effect(struct Effect *e, const struct City *c,
+                       struct City *c1) {
   c1->diplomatic_capacity += 1;
   c1->political_capacity += 1;
 }
 
-void coin_mint_tick_effect(const struct City *c, struct City *c1) {
+void coin_mint_tick_effect(struct Effect *e, const struct City *c,
+                           struct City *c1) {
   c1->gold_usage -= 0.5f;
 }
 
-void temple_of_jupiter_tick_effect(const struct City *c, struct City *c1) {
+void temple_of_jupiter_tick_effect(struct Effect *e, const struct City *c,
+                                   struct City *c1) {
   c1->diplomatic_capacity += 1;
 }
 
-void temple_of_mars_tick_effect(const struct City *c, struct City *c1) {
+void temple_of_mars_tick_effect(struct Effect *e, const struct City *c,
+                                struct City *c1) {
   c1->military_capacity += 1;
 }
 
-void pops_eating_tick_effect(const struct City *c, struct City *c1) {
+void pops_eating_tick_effect(struct Effect *e, const struct City *c,
+                             struct City *c1) {
   assert(c);
   assert(c1);
   const uint32_t consumption = c->population * 0.001f;
   c1->food_usage += consumption;
 }
 
-void colonia_capacity_tick_effect(const struct City *c, struct City *c1) {
+void colonia_capacity_tick_effect(struct Effect *e, const struct City *c,
+                                  struct City *c1) {
   static uint8_t lvl = 1;
   c1->political_capacity += 1;
 }
@@ -580,7 +608,8 @@ void colonia_capacity_tick_effect(const struct City *c, struct City *c1) {
 // fail or get the choice of moving the capital of the Roman Empire to your city
 // or move to Rome and then the game turns in a slow downward turn into decline?
 // Fun?
-void imperator_demands_money(const struct City *c, struct City *c1) {
+void imperator_demands_money(struct Effect *e, const struct City *c,
+                             struct City *c1) {
   if (timestep % 100 != 0) {
     return;
   }
@@ -616,15 +645,46 @@ void imperator_demands_money(const struct City *c, struct City *c1) {
   }
 }
 
+void building_tick_effect(struct Effect *e, const struct City *c,
+                          struct City *c1) {
+  struct Construction *arg = (struct Construction *)e->arg;
+
+  c1->gold -= arg->construction_cost; // FIXME: Gold usage instead?
+
+  // TODO: String in struct Effect must be free'd after Effect is done by simulate_..timestep
+  sprintf(e->description_str, "%llu days left, costing %.2f / day", e->duration, arg->construction_cost);
+
+  if (e->duration == 1) {
+    arg->maintained = true;
+    arg->construction_finished = true;
+  }
+}
+
 bool build_construction(struct City *c, struct Construction cp,
-                        struct Effect e) {
+                        struct Effect activated_effect) {
   if (c->gold >= cp.cost) {
-    c->gold -= cp.cost;
     struct Construction *construction = city_add_construction(c, cp);
+    construction->maintained = false;
+    construction->construction_cost =
+        construction->cost / construction->construction_time;
     construction->effect = calloc(1, sizeof(struct Effect));
-    construction->effect[0] =
-        e; // Linking the construction and its active effect
+    // Linking the construction and its active effect
+    construction->effect[0] = activated_effect;
     construction->num_effects = 1;
+
+    char *name_str = calloc(1, 64);
+    sprintf(name_str, "Building %s", cp.name_str);
+
+    char *description_str = calloc(1, 64);
+
+    struct Effect building_effect = {.name_str = name_str,
+                                     .description_str = description_str,
+                                     .duration = cp.construction_time,
+                                     .arg = construction,
+                                     .tick_effect = building_tick_effect};
+
+    city_add_effect(c, building_effect);
+
     return true;
   }
   return false;
@@ -754,14 +814,22 @@ void update_ui(const struct City *c) {
   row += 1;
   wmvclrprintw(root, row++, 0, "CONSTRUCTIONS");
   for (size_t i = 0; i < c->num_constructions; i++) {
-    wmvclrprintw(root, row++, 0, "%s: %s",
-                 c->constructions[i].effect[0].name_str,
-                 c->constructions[i].effect[0].description_str);
+    struct Construction *con = &c->constructions[i];
+    if (!con->construction_finished) {
+      continue;
+    }
+    if (con->maintained) {
+      wmvclrprintw(root, row++, 0, "%s: %s", con->effect[0].name_str,
+                   con->effect[0].description_str);
+    } else {
+      wmvclrprintw(root, row++, 0, "[UNMAINTAINED] %s: %s",
+                   con->effect[0].name_str, con->effect[0].description_str);
+    }
   }
 
   row += 1;
   wmvclrprintw(root, row++, 0, "RESOURCES");
-  wmvclrprintw(root, row++, 0, "Gold (%'2.f): %1.f kg", c->gold_usage, c->gold);
+  wmvclrprintw(root, row++, 0, "Gold (%'2.f): %2.f kg", c->gold_usage, c->gold);
   wmvclrprintw(root, row++, 0, "Food consumption: %'.1f kcal",
                c->food_production - c->food_usage);
   if (c->food_production - c->food_usage > 0.0f) {
@@ -834,7 +902,7 @@ int main() {
                                   .description_str =
                                       "Provides fresh water for the city.",
                                   .cost = 25.0f,
-                                  .maintenance = 1.0f,
+                                  .maintenance = 0.25f,
                                   .effect = &aqueduct_construction_effect,
                                   .num_effects = 1};
 
@@ -846,6 +914,7 @@ int main() {
 
   struct Construction farm = {
       .cost = 2.0f,
+      .construction_time = 7,
       .maintenance = 0.0f,
       .name_str = "Farm",
       .description_str = "Piece of land that can produce various crops.",
@@ -859,7 +928,8 @@ int main() {
       .tick_effect = basilica_tick_effect};
 
   struct Construction basilica = {.cost = 15.0f,
-                                  .maintenance = 0.5f,
+                                  .maintenance = 0.2f,
+                                  .construction_time = 3 * 30,
                                   .name_str = "Basilica",
                                   .description_str =
                                       "Public building used official matters.",
@@ -873,7 +943,8 @@ int main() {
                                              .tick_effect = forum_tick_effect};
 
   struct Construction forum = {.cost = 50.0f,
-                               .maintenance = 2.5f,
+                               .maintenance = 0.5f,
+                               .construction_time = 12 * 30,
                                .name_str = "Forum",
                                .description_str = "Public space for commerce.",
                                .effect = &forum_construction_effect,
@@ -882,17 +953,19 @@ int main() {
   struct Effect coin_mint_construction_effect = {
       .name_str = "Coin mint",
       .description_str =
-          "Coin mint produces coins, ensures commerce is not disrupted by war.",
+          "Produces coinage, ensures commerce is not disrupted by war.",
       .duration = FOREVER,
       .tick_effect = coin_mint_tick_effect};
 
-  struct Construction coin_mint = {.cost = 30.0f,
-                                   .maintenance = 2.5f,
-                                   .name_str = "Coin mint",
-                                   .description_str =
-                                       "Coin mint for production of coinage.",
-                                   .effect = &coin_mint_construction_effect,
-                                   .num_effects = 1};
+  struct Construction coin_mint = {
+      .cost = 30.0f,
+      .maintenance = 0.1f,
+      .construction_time = 2 * 30,
+      .name_str = "Coin mint",
+      .description_str =
+          "Produces coinage, ensures commerce is not disrupted by war.",
+      .effect = &coin_mint_construction_effect,
+      .num_effects = 1};
 
   const size_t num_temples = 2;
   struct Effect *temple_effects =
@@ -917,7 +990,8 @@ int main() {
       .name_str = "Temple",
       .description_str = "Used in sacrifies and other Roman traditions",
       .cost = 25.0f,
-      .maintenance = 1.5f,
+      .maintenance = 0.15f,
+      .construction_time = 4 * 30,
       .effect = temple_effects,
       .num_effects = num_temples};
 
@@ -992,6 +1066,7 @@ int main() {
       t0 = t1;
     }
 
+    wclear(root);
     update_ui(&cities[cidx]);
 
     // TODO: Mnemionc keybindings (E for effects, D for Dempgraphics, H for
