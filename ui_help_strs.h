21e7ac47f552bb1373ef53c560cfb3ff38bbf2a3
Alexander Lingtorp <alexander@lingtorp.com>
2019-09-04 16:43:26 +0200

    Added some Ui help strs

--- ui_help_strs.h
+++ ui_help_strs.h
@@ -11,20 +11,19 @@ enum LANGUAGES { en, NUM_LANGUAGES };
 
 // TODO: Adopt these conventions in main.c
 // ----- CONSTRUCTIONS -----
-const char *aqueduct_help_str[NUM_LANGUAGES] = {""};
+const char *aqueduct_help_str[NUM_LANGUAGES] = {"Provides water to baths and foundations."};
 const char *basilica_help_str[NUM_LANGUAGES] = {""};
-const char *farm_help_str[NUM_LANGUAGES] = {""};
-const char *senate_house_help_str[NUM_LANGUAGES] = {""};
-const char *temple_help_str[NUM_LANGUAGES] = {""};
-const char *coin_mint_help_str[NUM_LANGUAGES] = {""};
-const char *insula_help_str[NUM_LANGUAGES] = {""};
-const char *forum_help_str[NUM_LANGUAGES] = {""};
-const char *port_ostia_help_str[NUM_LANGUAGES] = {""};
-const char *bakery_help_str[NUM_LANGUAGES] = {""};
+const char *farm_help_str[NUM_LANGUAGES] = {"Produces food."};
+const char *senate_house_help_str[NUM_LANGUAGES] = {"Meeting place of the Senate. Enables Laws and increase political power."};
+const char *temple_help_str[NUM_LANGUAGES] = {"Increases various powers."};
+const char *coin_mint_help_str[NUM_LANGUAGES] = {"Reduces the negative effects of war. Increases gold income. Provides coinage."};
+const char *insula_help_str[NUM_LANGUAGES] = {"Multistory apartment building for the plebians."};
+const char *forum_help_str[NUM_LANGUAGES] = {"Public square."};
+const char *port_ostia_help_str[NUM_LANGUAGES] = {"Enables import and export of foodstuffs"};
+const char *bakery_help_str[NUM_LANGUAGES] = {"Increase the amount of food produces by farms."};
 const char *circus_maximus_help_str[NUM_LANGUAGES] = {""};
 const char *villa_publica_help_str[NUM_LANGUAGES] = {""};
-const char* bath_help_str[NUM_LANGUAGES] = {""};
-
+const char *bath_help_str[NUM_LANGUAGES] = {""};
 
 // ----- DESCRIPTION STRINGS -----
 const char *bath_description_strs[NUM_LANGUAGES] = {
